2e36b79e5c607bb87942cb33bd1a69f280977949
MertArar <85193329+MertArar@users.noreply.github.com>
2023-06-20 23:51:41 +0300

    ani no init addded

--- src/Containers/AnimatedPage/AnimatedCardNoInit.js
+++ src/Containers/AnimatedPage/AnimatedCardNoInit.js
@@ -1,7 +1,32 @@
+import { animate, motion } from "framer-motion";
 import React from "react";
 
-const AnimatedCardNoInit = () => {
-  return <div>AnimatedCardNoInit</div>;
+import styles from "./AnimatedCard.module.css";
+
+const animations = {
+  initial: { opacity: 1 },
+  animate: {
+    opacity: 1,
+    transition: { opacity: { type: "spring", duration: 2, bounce: 0.4 } },
+    exit: {
+      opacity: 0,
+      transition: { opacity: { type: "spring", duration: 2, bounce: 0.3 } },
+    },
+  },
+};
+
+const AnimatedCardNoInit = ({ children }) => {
+  return (
+    <motion.div
+      style={{ margin: 0, padding: 0 }}
+      variants={animations}
+      initial="initial"
+      animate="animate"
+      exit="exit"
+    >
+      {children}
+    </motion.div>
+  );
 };
 
 export default AnimatedCardNoInit;
