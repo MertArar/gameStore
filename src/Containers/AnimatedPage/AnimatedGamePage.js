997197ae65bfc6176bef1d161adfbc5291950e2d
MertArar <85193329+MertArar@users.noreply.github.com>
2023-06-20 23:58:09 +0300

    animation props added

--- src/Containers/AnimatedPage/AnimatedGamePage.js
+++ src/Containers/AnimatedPage/AnimatedGamePage.js
@@ -1,7 +1,31 @@
+import { motion } from "framer-motion";
 import React from "react";
 
-const AnimatedGamePage = () => {
-  return <div>AnimatedGamePage</div>;
+const animations = {
+  initial: { opacity: 0, x: -200 },
+  animate: {
+    opacity: 1,
+    x: 0,
+    transition: { x: { type: "spring", duration: 0.9, bounce: 0.4 } },
+  },
+  exit: {
+    opacity: 0,
+    x: -200,
+    transition: { x: { type: "tween", duration: 0.4, bounce: 0.3 } },
+  },
+};
+
+const AnimatedGamePage = ({ children }) => {
+  return (
+    <motion.div
+      variants={animations}
+      initial="initial"
+      animate="animate"
+      exit="exit"
+    >
+      {children}
+    </motion.div>
+  );
 };
 
 export default AnimatedGamePage;
--- src/Containers/AnimatedPage/AnimatedLike.js
+++ src/Containers/AnimatedPage/AnimatedLike.js
@@ -1,7 +1,30 @@
+import { motion } from "framer-motion";
 import React from "react";
 
-const AnimatedLike = () => {
-  return <div>AnimatedLike</div>;
+const animations = {
+  initial: { scaleY: 1, scaleX: 1, fill: "#999" },
+  animate: {
+    scaleY: 1.225,
+    scaleX: 1.225,
+    fill: "#FD2727",
+    transition: {
+      scaleY: { type: "spring", duration: 1.25, bounce: 0.4 },
+      scaleX: { type: "spring", duration: 1.25, bounce: 0.4 },
+    },
+  },
+};
+
+const AnimatedLike = ({ children }) => {
+  return (
+    <motion.div
+      style={{ margin: 0, padding: 0 }}
+      variants={animations}
+      initial="initial"
+      animate="animate"
+    >
+      {children}
+    </motion.div>
+  );
 };
 
 export default AnimatedLike;
